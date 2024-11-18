81b1ce4be2f9d3f7b9c946c5951d0acd21874af0
thebuildcraft <104298589+thebuildcraft@users.noreply.github.com>
2024-11-18 21:39:50 +0100

    version 3.2.1

--- common/src/main/java/de/the_build_craft/remote_player_waypoints_for_xaero/common/AbstractModInitializer.java
+++ common/src/main/java/de/the_build_craft/remote_player_waypoints_for_xaero/common/AbstractModInitializer.java
@@ -48,13 +48,13 @@ import java.util.Timer;
  *
  * @author James Seibel
  * @author Leander Knüttel
- * @version 04.09.2024
+ * @version 18.11.2024
  */
 public abstract class AbstractModInitializer
 {
 	public static final String MOD_ID = "remote_player_waypoints_for_xaero";
 	public static final String MOD_NAME = "Remote Player Waypoints For Xaero's Map";
-	public static final String VERSION = "3.0.0";
+	public static final String VERSION = "3.2.1";
 	public static final Logger LOGGER = LogManager.getLogger("RemotePlayerWaypointsForXaero");
 	public static AbstractModInitializer INSTANCE;
 
